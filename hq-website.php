f64f689a47d888aa26c68c230264a58fe31676fc
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-04-12 23:36:16 -0400

    structure react

---
+++ hq-website.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: 	HQ Website Plugin
+Plugin URI:		http://wordpress.org/plugins/svg-support/
+Description: 	HQ Rental Plugin
+Version: 		1.0.0
+Author: 		HQ Rental Software.com
+Author URI: 	http://hqrentalsoftware.com
+Text Domain: 	hq-website
+*/
+
+require_once ('shortcodes/hq-trial-form.php');
