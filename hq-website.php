976720b90c0933564874a596e5141d68ee3b6747
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-04-13 23:34:42 -0400

    ver

--- hq-website.php
+++ hq-website.php
@@ -3,7 +3,7 @@
 Plugin Name: 	HQ Website Plugin
 Plugin URI:		http://wordpress.org/plugins/svg-support/
 Description: 	HQ Rental Plugin
-Version: 		0.0.5
+Version: 		0.0.6
 Author: 		HQ Rental Software.com
 Author URI: 	http://hqrentalsoftware.com
 Text Domain: 	hq-website
